1891ed0f1a8723c97d8f6bb2068b7ec164b88c58
Andyiev <ayevdokymenmk@gmail.com>
2021-08-02 13:32:10 -0600

    Change users schema. Add -insert- of a new user to the database

--- server/routes/users.js
+++ server/routes/users.js
@@ -22,20 +22,20 @@ module.exports = (db) => {
     console.log(req.body);
     const {firstName, lastName, email} = req.body;
     console.log("This is first name, last name and email", firstName, lastName, email);
-    // db.query(
-    //   `
-    //   INSERT INTO users (first_name, last_name, email) 
-    //   VALUES ($1, $2, $3)`, [firstName, lastName, email]
-    // )
+    db.query(
+      `
+      INSERT INTO users (first_name, last_name, email) 
+      VALUES ($1, $2, $3)`, [firstName, lastName, email]
+    )
     
-    //   .then((response) => {
+      .then((response) => {
        
-    //     res.json({ users: response.rows });
-    //   })
-    //   .catch((err) => {
-    //     console.log("Error:", err);
-    //     res.json({ users: err });
-    //   });
+        res.json({ users: response.rows });
+      })
+      .catch((err) => {
+        console.log("Error:", err);
+        res.json({ users: err });
+      });
   });
 
   return router;
