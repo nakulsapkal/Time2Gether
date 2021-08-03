2b1fa2c2c407469847dfb03f7bdb5dc4b5c94875
Andyiev <ayevdokymenmk@gmail.com>
2021-08-03 15:06:04 -0600

    Add code to INSERT new business user

--- server/routes/business_users.js
+++ server/routes/business_users.js
@@ -13,5 +13,20 @@ module.exports = (db) => {
         res.json({ businessUsers: err });
       });
   });
+
+  // Route to add a new Business user into the database
+  router.post("/users/signup", function (req, res) {
+    //console.log("This is business user data from business_users.js", customData.first_name);
+    const { businessName, ownerName, registrationNumber, email, phoneNumber, password } = req.body;
+    db.query(
+      `
+      INSERT INTO users (business_name, owner_name, email, password, registration_number, phone_number) 
+      VALUES ($1, $2, $3, $4, $5, $6)`, [businessName, ownerName, email, password, registrationNumber,phoneNumber]
+    )
+      .then(data => {
+        res.json(data.rows[0]);
+      })
+      .catch(error => console.log(error));
+  })
   return router;
 };
