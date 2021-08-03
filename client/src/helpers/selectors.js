2c6c1495f27c4af1c6b64e68016924b8319f4252
Nakul Sapkal <nakul.sapkal@gmail.com>
2021-08-03 13:34:39 -0400

    add new helper functions

--- client/src/helpers/selectors.js
+++ client/src/helpers/selectors.js
@@ -6,4 +6,42 @@ function getActivityByUser(activities) {
   let user = localStorage.getItem("user");
   return activities.find((actObj) => actObj.user_id === user.id);
 }
-export { getActivityById, getActivityByUser };
+
+function getActivityCreatedByUser(id, activities) {
+  let user = localStorage.getItem("user");
+  return activities.filter(
+    (actObj) => actObj.user_id === user.id && actObj.joined_at === null
+  );
+}
+
+function getActivityJoinedByUser(id, activities) {
+  let user = localStorage.getItem("user");
+  return activities.filter(
+    (actObj) => actObj.user_id === user.id && actObj.joined_at !== null
+  );
+}
+
+function getActivityHistoryForUser(id, activities) {
+  let user = localStorage.getItem("user");
+  let date = new Date();
+
+  return activities.filter(
+    (actObj) => actObj.user_id === user.id && actObj.end_date < date
+  );
+}
+
+function getActivitiesFavouriteByUser(id, activities) {
+  let user = localStorage.getItem("user");
+  return activities.filter(
+    (actObj) => actObj.user_id === user.id && actObj.favourite === true
+  );
+}
+
+export {
+  getActivityById,
+  getActivityByUser,
+  getActivityJoinedByUser,
+  getActivityCreatedByUser,
+  getActivitiesFavouriteByUser,
+  getActivityHistoryForUser,
+};
