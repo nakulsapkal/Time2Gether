ce1fd1ac389779013a7188d6d6365c38b4e27a16
ysycecilia <ysycecilia@gmail.com>
2021-08-03 20:31:37 -0400

    change state initial type from obj to array

--- client/src/hooks/useApplicationData.js
+++ client/src/hooks/useApplicationData.js
@@ -1,14 +1,14 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+
 //This hook is to manage data for application
 export default function useApplicationData(params) {
   //State Declaration and initialize it as an object
   const [state, setState] = useState({
-    users: {},
-    activities: {},
+    users: [],
+    activities: []
   });
-
   //This useEffect is ran only once at the initial app start to fetch the data (async) from API via axios
   useEffect(() => {
     const p1 = axios.get("/api/users");
@@ -22,7 +22,7 @@ export default function useApplicationData(params) {
       setState((prev) => ({
         ...prev,
         users: first.data.users,
-        activities: second.data.activities,
+        activities: second.data.activities
       }));
     });
   }, []);
@@ -55,6 +55,8 @@ export default function useApplicationData(params) {
         }
       });
   }
+
+
 
   return { state, addUser, validateUser };
 }
