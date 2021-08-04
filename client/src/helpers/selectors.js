c2aba73a8b36d669c8bae6eda23a27588796f936
Nakul Sapkal <nakul.sapkal@gmail.com>
2021-08-03 20:10:19 -0400

    add selector function for activities

--- client/src/helpers/selectors.js
+++ client/src/helpers/selectors.js
@@ -1,46 +1,48 @@
+function dateFormatter(date) {
+  return date.toISOString().slice(0, 10);
+}
+
 function getActivityById(id, activities) {
   return activities.filter((a) => a.id === id);
 }
 
-function getActivityByUser(activities) {
-  let user = localStorage.getItem("user");
-  return activities.find((actObj) => actObj.user_id === user.id);
+function getActivityByUser(id, activities) {
+  return activities.find((actObj) => actObj.user_id === id);
 }
 
 function getActivityCreatedByUser(id, activities) {
-  let user = localStorage.getItem("user");
   return activities.filter(
-    (actObj) => actObj.user_id === user.id && actObj.joined_at === null
+    (actObj) => actObj.user_id === id && actObj.joined_at === null
   );
 }
 
-function getActivityJoinedByUser(id, activities) {
-  let user = localStorage.getItem("user");
+function getUpcomingActivityForUser(id, activities) {
+  let date = new Date().toISOString().slice(0, 10);
   return activities.filter(
-    (actObj) => actObj.user_id === user.id && actObj.joined_at !== null
+    (actObj) =>
+      actObj.user_id === id &&
+      actObj.joined_at !== null &&
+      actObj.start_date > date
   );
 }
 
 function getActivityHistoryForUser(id, activities) {
-  let user = localStorage.getItem("user");
-  let date = new Date();
-
+  let date = new Date().toISOString().slice(0, 10);
   return activities.filter(
-    (actObj) => actObj.user_id === user.id && actObj.end_date < date
+    (actObj) => actObj.user_id === id && actObj.end_date < date
   );
 }
 
 function getActivitiesFavouriteByUser(id, activities) {
-  let user = localStorage.getItem("user");
   return activities.filter(
-    (actObj) => actObj.user_id === user.id && actObj.favourite === true
+    (actObj) => actObj.user_id === id && actObj.favourite === true
   );
 }
 
 export {
   getActivityById,
   getActivityByUser,
-  getActivityJoinedByUser,
+  getUpcomingActivityForUser,
   getActivityCreatedByUser,
   getActivitiesFavouriteByUser,
   getActivityHistoryForUser,
