eaf86df0dac4a995cb46aa4cb5107ff9e442b2b0
ysycecilia <ysycecilia@gmail.com>
2021-08-03 20:26:26 -0400

    add post url to create a new activity

--- server/routes/activities.js
+++ server/routes/activities.js
@@ -17,6 +17,43 @@ module.exports = (db) => {
       });
   });
 
+  router.post("/activities/create", (req, res) => {
+    const {
+      img, details, loginUserId, category,
+      start_date, end_date, start_time, end_time,  
+      street_number, street_name, city, province, postal_code
+    } = req.body.body;
+    // console.log("body content line 27",req.body.body)
+ 
+    const categoryList = {1: "Outdoor sports", 2: "Baking", 3: "Indoor sports", 4: "Cooking"}
+
+    const categoryId = Object.keys(categoryList).find(c => categoryList[c] == category);
+
+    // console.log("categoryId: ", categoryId);
+
+    db.query(
+    
+          `INSERT INTO address (street_number, street_name, city, province, postal_code)
+
+          VALUES ($1, $2, $3, $4, $5) RETURNING *`, [street_number, street_name, city, province, postal_code]
+        )
+          .then(data => {
+          // console.log("##############", data.rows[0].id)
+          const address_id = data.rows[0].id;
+          db.query(
+            ` INSERT INTO activities 
+              (img, details, user_id, category_id,
+              start_date, end_date, start_time, end_time, address_id) 
+    
+              VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *`, [img, details, loginUserId,categoryId, start_date, end_date, start_time, end_time, address_id]
+          ).then(data1 => {
+            res.json({address : data.rows[0], activities : data1.rows[0]});
+          }).catch(error => console.log(error));
+        
+        })
+          .catch(error => console.log(error));
+    }
+  );
 
 
   return router;
