265c13297fb02c136e87a3cbd2f732ba209d71bd
ysycecilia <ysycecilia@gmail.com>
2021-08-04 21:44:31 -0400

    modify query to filter out passed act and sort asc

--- server/routes/activities.js
+++ server/routes/activities.js
@@ -6,13 +6,14 @@ module.exports = (db) => {
   router.get("/activities", (req, res) => {
     // console.log("Activity Router:", db);
     db.query(
-      `SELECT activities.*, address.*, categories.name as category from activities
+      `SELECT activities.*, address.street_number, address.street_name, address.city, address.province, address.postal_code, categories.name as category from activities
        JOIN address ON address.id = activities.address_id
-       JOIN categories ON activities.category_id = categories.id;`
+       JOIN categories ON activities.category_id = categories.id
+       WHERE  start_date > now()
+       ORDER BY start_date asc;`
     )
-      .then((result) => {
-        // console.log("One Activity record from activities:", result.rows[0]);
-        res.json({ activities: result.rows });
+      .then(data => {
+        res.json({ activities: data.rows });
       })
       .catch((err) => {
         console.log("Error from activities route: ", err);
@@ -42,48 +43,40 @@ module.exports = (db) => {
       start_date, end_date, start_time, end_time,  
       street_number, street_name, city, province, postal_code
     } = req.body.body;
-    // console.log("body content line 27",req.body.body)
-    console.log("categoryId: ", category);
+
     const categoryList = {1: "Outdoor sports", 2: "Baking", 3: "Indoor sports", 4: "Cooking"}
 
     const categoryId = Object.keys(categoryList).find(c => categoryList[c] == category);
 
-    console.log("categoryId: ", categoryId);
-
     db.query(
+      `INSERT INTO address (street_number, street_name, city, province, postal_code)
+
+      VALUES ($1, $2, $3, $4, $5) RETURNING *`, [street_number, street_name, city, province, postal_code]
+    ).then(data => {
+      const address_id = data.rows[0].id;
+
+        db.query(
+          ` INSERT INTO activities 
+            (img, details, category_id, created_at,
+            start_date, end_date, start_time, end_time, address_id) 
+  
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *`, 
+            [img, details, categoryId, new Date().toISOString().slice(0, 10) ,start_date, end_date, start_time, end_time, address_id]
+        ).then(data1 => {
+          const activity_id = data1.rows[0].id;
+          db.query(
+            ` INSERT INTO user_activity 
+              (user_id, activity_id, joined_at) 
     
-          `INSERT INTO address (street_number, street_name, city, province, postal_code)
+              VALUES ($1, $2, $3) RETURNING *`, [loginUserId, activity_id, null]
+          )
+        }).then(data2 => {
+                res.json({address : data.rows[0]});
+        }).catch(error => console.log(error));
 
-          VALUES ($1, $2, $3, $4, $5) RETURNING *`, [street_number, street_name, city, province, postal_code]
-        )
-          .then(data => {
-          // console.log("##############", data.rows[0].id)
-          const address_id = data.rows[0].id;
-            db.query(
-              ` INSERT INTO activities 
-                (img, details, category_id, created_at,
-                start_date, end_date, start_time, end_time, address_id) 
-      
-                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *`, 
-                [img, details, categoryId, new Date().toISOString().slice(0, 10) ,start_date, end_date, start_time, end_time, address_id]
-            ).then(data1 => {
-              const activity_id = data1.rows[0].id;
-              db.query(
-                ` INSERT INTO user_activity 
-                  (user_id, activity_id, joined_at) 
-        
-                  VALUES ($1, $2, $3) RETURNING *`, [loginUserId, activity_id, null]
-              )
-            }).then(data2 => {
-              // console.log("hi",data1)
-              res.json({address : data.rows[0]});
-            }).catch(error => console.log(error));
-        
-        })
-          .catch(error => console.log(error));
+    }).catch(error => console.log(error));
     }
-  );
-
+  ); 
 
   return router;
 };
