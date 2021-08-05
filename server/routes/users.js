2813795746ce8066cc1d5eeabff13ec98c6a9277
ysycecilia <ysycecilia@gmail.com>
2021-08-04 21:45:20 -0400

    create post & put user routes for join/drop an act

--- server/routes/users.js
+++ server/routes/users.js
@@ -31,5 +31,43 @@ module.exports = (db) => {
       .catch((error) => console.log(error));
   });
 
+  // router.get("/user/joineded", (req, res) => {
+  //   db.query( `SELECT * FROM user_activity;`
+  //     ).then(data => {
+  //       res.json(data.rows[0]);
+  //     }).catch(error => console.log(error));
+  // })
+
+  // add a new "join" event to user-activity table
+  router.post("/users/joined", (req, res) => {
+    const { joined_at, user_id, activity_id } = req.body.body;
+    console.log("joined data ******************", req.body.body) 
+    // console.log("user_id******************",values.user_id) 
+    // console.log("activity_id******************",values.activity_id) 
+
+    db.query( `
+      INSERT INTO user_activity (joined_at, user_id, activity_id) VALUES ($1, $2, $3)
+      RETURNING *`, [new Date(), user_id, activity_id]
+      // .toISOString().slice(0, 10)
+    ).then(data => {
+          res.json(data.rows);
+        }).catch(error => console.log(error));
+  })
+
+  // cancel a "join" event to user-activity table
+  router.put("/users/joined", (req, res) => {
+    const { user_id, activity_id } = req.body.body;
+
+    db.query( `DELETE FROM user_activity 
+      WHERE user_id = $1
+      AND activity_id = $2
+      RETURNING *`, [user_id, activity_id]
+    ).then(data => {
+          res.json(data.rows[0]);
+        }).catch(error => console.log(error));
+  })
+
+
+
   return router;
 };
