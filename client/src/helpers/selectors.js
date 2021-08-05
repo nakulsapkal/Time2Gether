d9d2993bf5eef424e09269feadf14df464e373ae
ysycecilia <ysycecilia@gmail.com>
2021-08-04 21:40:53 -0400

    create get logged userid func

--- client/src/helpers/selectors.js
+++ client/src/helpers/selectors.js
@@ -7,6 +7,12 @@ function getActivityById(id, activities) {
 //   return loginUser? true : false;
 // }
 
+function getLoggedUserId() {
+  const loginUser = JSON.parse(localStorage.getItem('userData'));
+  const loginUserId = loginUser.id
+  return loginUserId;
+}
+
 function getActivityByUser(id, activities) {
   return activities.find((actObj) => actObj.user_id === id);
 }
@@ -40,11 +46,21 @@ function getActivitiesFavouriteByUser(id, activities) {
   );
 }
 
+function getJoinedTime(id, act_id, activities) {
+    
+  const act = act_id && activities.find(
+    (actObj) => actObj.user_id === id && actObj.activity_id === act_id
+    );
+  return act ? act.joined_at : null;
+}
+
 export {
   getActivityById,
+  getLoggedUserId,
   getActivityByUser,
   getUpcomingActivityForUser,
   getActivityCreatedByUser,
   getActivitiesFavouriteByUser,
   getActivityHistoryForUser,
+  getJoinedTime,
 };
