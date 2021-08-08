add296f88c622c1d330672ad7800212674977292
ysycecilia <ysycecilia@gmail.com>
2021-08-08 12:07:03 -0400

    create schema and seed for new tables(messages & conversations

---
+++ server/db/schema/messages.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS messages CASCADE;
+CREATE TABLE messages (
+	id SERIAL PRIMARY KEY,
+  senderId INTEGER NOT NULL,
+  content TEXT NOT NULL,
+  readStatus BOOLEAN DEFAULT FALSE,
+  created_at TIMESTAMP NOT NULL,
+  conversationId INTEGER REFERENCES conversations(id) ON DELETE CASCADE
+);
---
+++ server/db/seeds/messages.sql
@@ -0,0 +1,5 @@
+INSERT INTO messages 
+(senderId, content, readStatus, conversationId, created_at)
+VALUES (1,'Hi, I really like your activity!', TRUE, 1, CURRENT_TIMESTAMP), (2,'Thanks! Are you coming?', TRUE, 1, CURRENT_TIMESTAMP),
+(1,'Absolutely! See you then.', TRUE, 1, CURRENT_TIMESTAMP)
+returning *;
