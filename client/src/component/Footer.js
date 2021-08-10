4f43a6665c2bdc22919c02036808fcbd14ee942e
Andyiev <ayevdokymenmk@gmail.com>
2021-08-10 16:16:48 -0600

    Add footer and styling for footer

--- client/src/component/Footer.js
+++ client/src/component/Footer.js
@@ -1,3 +1,10 @@
-export default function Footer(props) {
-  return <></>;
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="footer" >
+      <p>Time2Gether Inc. Ltd. © 2021 All rights reserved.</p>
+    </div>
+  )
 }
